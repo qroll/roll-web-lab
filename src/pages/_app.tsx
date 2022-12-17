eadb075fa57fc17be7e65e3586b11bda766dcb16
roll <quek.ruoling@gmail.com>
2022-12-17 14:16:20 +0800

    Set up fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
+import localFont from "@next/font/local";
 import type { AppProps } from "next/app";
 import { ThemeSelector } from "../modules/theme/theme-selector";
 import Navbar from "../modules/navbar/navbar";
+
+const font = localFont({ src: "../../assets/SourceSans3-VariableFont_wght.ttf", weight: "100 900" });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeSelector>
       <Navbar />
+      <style jsx global>{`
+        :root {
+          --font-base: ${font.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </ThemeSelector>
   );
